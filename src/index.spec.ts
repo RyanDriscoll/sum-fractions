a23c31af7823cab61f71cc3e7c5fed2803bb2bc1
Ryan Driscoll <driscollrp@gmail.com>
2024-03-03 09:56:50 -0700

    feat: add parseEquation function with tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,8 +1,27 @@
-import { addFractions } from './index.ts';
+import { addFractions, parseEquation } from './index.ts';
 
 describe('adding fractions', () => {
   it('adds fractions', () => {
     const result = addFractions(['1/2+2/3', '1/10+5/50']);
     expect(result).toEqual(['7/6', '2/10']);
+  });
+});
+
+describe('parseEquation', () => {
+  it('returns an array of Fraction objects', () => {
+    const result = parseEquation('1/2+2/3');
+    expect(result).toEqual([
+      { numerator: 1, denominator: 2 },
+      { numerator: 2, denominator: 3 },
+    ]);
+  });
+
+  it('can handle more than two fractions', () => {
+    const result = parseEquation('1/2+2/3+9/10');
+    expect(result).toEqual([
+      { numerator: 1, denominator: 2 },
+      { numerator: 2, denominator: 3 },
+      { numerator: 9, denominator: 10 },
+    ]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,14 @@ import { Fraction } from './types.js';
 export function addFractions(fractions: string[]): string[] {
   return [];
 }
+
+export function parseEquation(equation: string): Fraction[] {
+  const fractions = equation.split('+');
+  return fractions.map((fraction) => {
+    const [numerator, denominator] = fraction.split('/').map(Number);
+    return {
+      numerator,
+      denominator,
+    };
+  });
+}
